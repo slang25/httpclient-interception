a776327d8c1de00e7ad2037e896c171c91c239f6
Stuart Lang <stuart.b.lang@gmail.com>
2019-07-04 12:14:18 +0100

    Show mono bug sample

---
+++ samples/MonoBug/MonoBugTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using JustEat.HttpClientInterception;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MonoBug
+{
+    public sealed class MonoBugTest
+    {
+        [Fact]
+        public async Task Test()
+        {
+            var options = new HttpClientInterceptorOptions();
+
+            var builder = new HttpRequestInterceptionBuilder()
+                .Requests()
+                .ForHost("public.je-apis.com")
+                .ForPath("terms")
+                .Responds()
+                .WithJsonContent(new { Id = 1, Link = "https://www.just-eat.co.uk/privacy-policy" })
+                .RegisterWith(options);
+
+            var client = options.CreateHttpClient();
+
+            // The value of json will be "{\"Id\":1,\"Link\":\"https://www.just-eat.co.uk/privacy-policy\"}"
+            var json = await client.GetStringAsync("http://public.je-apis.com/terms");
+        }
+    }
+}
